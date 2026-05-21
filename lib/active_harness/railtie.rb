a47a65c3e8c1e3e5cff44df83b401f98c37e92ea
Ilya N. Zykin <zykin-ilya@ya.ru>
2026-05-21 13:51:47 +0300

    v0.2.6

--- lib/active_harness/railtie.rb
+++ lib/active_harness/railtie.rb
@@ -2,7 +2,7 @@ module ActiveHarness
   class Railtie < Rails::Railtie
     APP_AI_DIRS = %w[agents prompts tribunals pipelines memory].freeze
 
-    initializer "active_harness.autoload_paths" do |app|
+    initializer "active_harness.autoload_paths", before: :set_autoload_paths do |app|
       APP_AI_DIRS.each do |dir|
         path = Rails.root.join("app", "ai", dir)
         app.config.autoload_paths << path.to_s if path.exist?
